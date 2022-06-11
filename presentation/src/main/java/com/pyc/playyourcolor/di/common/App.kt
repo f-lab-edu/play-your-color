8fae8a51c81e051572d9eba096d0dc5c8f01e216
Jeong-heon2 <cjhgg2@gmail.com>
2022-06-11 16:01:27 +0900

    build-#4 : add hilt dependency at presentation module

---
+++ presentation/src/main/java/com/pyc/playyourcolor/di/common/App.kt
@@ -0,0 +1,8 @@
+package com.pyc.playyourcolor.di.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
--- presentation/src/main/java/com/pyc/playyourcolor/playlist/view/PlayListActivity.kt
+++ presentation/src/main/java/com/pyc/playyourcolor/playlist/view/PlayListActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pyc.playyourcolor.playlist.view.ui.theme.PlayYourColorTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PlayListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
