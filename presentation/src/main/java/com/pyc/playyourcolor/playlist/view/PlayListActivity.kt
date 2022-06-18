7418844bd38c74b7357a056e9f4403f3546465ef
Jeong-heon2 <cjhgg2@gmail.com>
2022-06-18 18:05:52 +0900

    feat-#7: 화면 이동 적용

--- presentation/src/main/java/com/pyc/playyourcolor/playlist/view/PlayListActivity.kt
+++ presentation/src/main/java/com/pyc/playyourcolor/playlist/view/PlayListActivity.kt
@@ -3,15 +3,19 @@ package com.pyc.playyourcolor.playlist.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.pyc.playyourcolor.playlist.view.ui.colorplaylist.ColorPlayListScreen
+import com.pyc.playyourcolor.playlist.view.ui.components.PlayListTabRow
+import com.pyc.playyourcolor.playlist.view.ui.primaryplaylist.PrimaryPlayListScreen
+import com.pyc.playyourcolor.playlist.view.ui.settings.SettingsScreen
 import com.pyc.playyourcolor.playlist.view.ui.theme.PlayYourColorTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,5 +36,38 @@ fun PlayYourColorApp() {
         val navController = rememberNavController()
         val backstackEntry = navController.currentBackStackEntryAsState()
         val currentScreen = PlayListScreen.fromRoute(backstackEntry.value?.destination?.route)
+
+        Scaffold(
+            topBar = {
+                PlayListTabRow(
+                    allScreens = allScreens,
+                    onTabSelected = { screen ->
+                        navController.navigate(screen.name)
+                    },
+                    currentScreen = currentScreen
+                )
+            }
+        ) { innerPadding ->
+            NaveHost(navController, modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
+
+@Composable
+fun NaveHost(navController: NavHostController, modifier: Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = PlayListScreen.Primary.name,
+        modifier = modifier
+    ) {
+        composable(PlayListScreen.Primary.name) {
+            PrimaryPlayListScreen()
+        }
+        composable(PlayListScreen.Color.name) {
+            ColorPlayListScreen()
+        }
+        composable(PlayListScreen.Settings.name) {
+            SettingsScreen()
+        }
     }
 }
---
+++ presentation/src/main/java/com/pyc/playyourcolor/playlist/view/ui/primaryplaylist/PrimaryPlayListScreen.kt
@@ -0,0 +1,9 @@
+package com.pyc.playyourcolor.playlist.view.ui.primaryplaylist
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PrimaryPlayListScreen() {
+    Text(text = "기본 재생 화면")
+}
