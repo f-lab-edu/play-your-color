52cda3ced9228d1bc54fa31ba8b813e361e94069
Jeong-heon2 <cjhgg2@gmail.com>
2022-07-09 14:34:10 +0900

    feat-#21: 편집화면의 audio model인 AudioEditModel 작성

--- presentation/src/main/java/com/pyc/playyourcolor/editor/model/AudioEditModel.kt
+++ presentation/src/main/java/com/pyc/playyourcolor/editor/model/AudioEditModel.kt
@@ -3,6 +3,7 @@ package com.pyc.playyourcolor.editor.model
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import pyc.domain.model.PlaylistItem
 
 class AudioEditModel(
     val id: Int,
@@ -15,3 +16,13 @@ class AudioEditModel(
 ) {
     var checked: Boolean by mutableStateOf(initialChecked)
 }
+
+fun toAudioEditModel(playlistItem: PlaylistItem): AudioEditModel =
+    AudioEditModel(
+        id = playlistItem.id,
+        uri = playlistItem.audio.uri,
+        title = playlistItem.audio.title,
+        artist = playlistItem.audio.artist,
+        imgUri = playlistItem.audio.imgUri,
+        playPossible = playlistItem.audio.playPossible
+    )
