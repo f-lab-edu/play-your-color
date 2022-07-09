74dec2ba5481566cdd1f9a37a844879ab7e0e468
Jeong-heon2 <cjhgg2@gmail.com>
2022-07-09 14:35:24 +0900

    feat-#21: PlaylistEditorViewModel 작성

--- presentation/src/main/java/com/pyc/playyourcolor/editor/viewmodel/PlaylistEditorViewModel.kt
+++ presentation/src/main/java/com/pyc/playyourcolor/editor/viewmodel/PlaylistEditorViewModel.kt
@@ -1,19 +1,135 @@
 package com.pyc.playyourcolor.editor.viewmodel
 
+import androidx.compose.material.BottomSheetState
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.pyc.playyourcolor.common.move
 import com.pyc.playyourcolor.editor.model.AudioEditModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import com.pyc.playyourcolor.editor.model.toAudioEditModel
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import pyc.domain.usecase.DeleteAudioUseCase
+import pyc.domain.usecase.GetPlaylistUseCase
+import pyc.domain.usecase.UpdatePlaylistAudioOrderUseCase
 
-@HiltViewModel
-class PlaylistEditorViewModel @Inject constructor(
-
+class PlaylistEditorViewModel @AssistedInject constructor(
+    private val getPlaylistUseCase: GetPlaylistUseCase,
+    private val updatePlaylistAudioOrderUseCase: UpdatePlaylistAudioOrderUseCase,
+    private val deleteAudioUseCase: DeleteAudioUseCase,
+    @Assisted private val playlistId: Int,
 ) : ViewModel() {
 
-    private var _liveAudioEditModelList = mutableListOf<AudioEditModel>()
-    val liveAudioEditModelList : List<AudioEditModel> get() = _liveAudioEditModelList
+    private var _audioEditModelList = mutableStateListOf<AudioEditModel>()
+    val audioEditModelList : List<AudioEditModel> get() = _audioEditModelList
 
-    fun getAudioEditModelList(id: Int) {
+    private var _selectedCountState = mutableStateOf(0)
+    val selectedCountState : State<Int> get() = _selectedCountState
 
+    @OptIn(ExperimentalMaterialApi::class)
+    val bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    init {
+        getAudioEditModelList(playlistId)
+    }
+
+    private fun getAudioEditModelList(id: Int) {
+        val disposable = getPlaylistUseCase(id)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe (
+                { _audioEditModelList.addAll(it.map { item -> toAudioEditModel(item) }) },
+                { throwable -> },
+                {})
+        compositeDisposable.add(disposable)
+    }
+
+    fun audioMove(from: Int, to: Int) {
+        updatePlaylistAudioOrderUseCase(playlistId, _audioEditModelList.map { it.id }.toMutableList(), from, to)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnComplete {
+                _audioEditModelList.move(from, to)
+            }
+            .subscribe()
+    }
+
+    fun selectAudio(idx: Int) {
+        if (_audioEditModelList[idx].checked) {
+            _audioEditModelList[idx].checked = false
+            _selectedCountState.value--
+        } else {
+            _audioEditModelList[idx].checked = true
+            _selectedCountState.value++
+        }
+    }
+
+    fun deselectAllAudio() {
+        _audioEditModelList.forEach {
+            if (it.checked) {
+                it.checked = false
+                _selectedCountState.value--
+            }
+        }
+    }
+
+    fun deleteSelectedAudio() {
+        deleteAudioUseCase(
+            playlistId,
+            _audioEditModelList.map { it.id },
+            _audioEditModelList.filter { it.checked }.map { item -> item.id })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnComplete {
+                _audioEditModelList.removeAll { it.checked }
+            }
+            .subscribe()
+    }
+
+    fun selectAllAudio() {
+        if (_selectedCountState.value < _audioEditModelList.size) {
+            _audioEditModelList.forEach {
+                if (!it.checked) {
+                    it.checked = true
+                    _selectedCountState.value++
+                }
+            }
+        } else {
+            _audioEditModelList.forEach {
+                it.checked = false
+                _selectedCountState.value--
+            }
+        }
+    }
+
+    @AssistedFactory
+    interface IdAssistedFactory {
+        fun create(id: Int): PlaylistEditorViewModel
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: IdAssistedFactory,
+            id: Int
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return assistedFactory.create(id) as T
+            }
+        }
     }
 }
