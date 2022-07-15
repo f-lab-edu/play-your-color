214375380735134fa23a0a00ea9bbe429fc1d711
stella <hyunjungqss@gmail.com>
2022-07-15 14:14:18 +0900

    fix-#27: override hashCode and equals in PlaylistItem Class

--- domain/src/main/java/pyc/domain/model/PlaylistItem.kt
+++ domain/src/main/java/pyc/domain/model/PlaylistItem.kt
@@ -3,4 +3,14 @@ package pyc.domain.model
 data class PlaylistItem(
     val id: Int,
     val audio: Audio
-)
+) {
+
+    override fun hashCode(): Int {
+        return id.hashCode() * 31
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if(other == null || other !is PlaylistItem) return false
+        return this.id == other.id
+    }
+}
