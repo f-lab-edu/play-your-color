fa4d3be76eb20bd5fb05b636ef45fa21d8d7d056
hyun17171771 <hyunjungqss@gmail.com>
2022-08-17 15:02:20 +0900

    feat-#55: colorInfo repository 및 관련 usecase 추가

---
+++ domain/src/main/java/pyc/domain/repository/ColorInfoRepository.kt
@@ -0,0 +1,18 @@
+package pyc.domain.repository
+
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import pyc.domain.model.ColorInfo
+import pyc.domain.model.ColorInfoListItem
+
+interface ColorInfoRepository {
+    fun getColorInfoList() : Flowable<List<ColorInfoListItem>>
+
+    fun deleteInfoColor(
+        colorInfoId: Int
+    ) : Completable
+
+    fun insertColorInfo(
+        colorInfo: ColorInfo
+    ) : Completable
+}
