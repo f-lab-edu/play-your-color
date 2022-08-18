2e4dcdfd539e77e4c0c7fdaadb6d222e993ca5b0
hyun17171771 <hyunjungqss@gmail.com>
2022-08-18 16:31:18 +0900

    feat-#55: ColorInfoRepositoryImpl 추가 구현 및 repository 모듈 추가

--- data/src/main/java/com/pyc/data/di/RepositoryModule.kt
+++ data/src/main/java/com/pyc/data/di/RepositoryModule.kt
@@ -1,16 +1,24 @@
 package com.pyc.data.di
 
+import com.pyc.data.repository.ColorInfoRepositoryImpl
 import com.pyc.data.repository.LastPlayerStatusInformationRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import pyc.domain.repository.ColorInfoRepository
 import pyc.domain.repository.LastPlayerStatusInformationRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    internal abstract fun bindColorInfoRepository(
+        colorInfoRepositoryImpl: ColorInfoRepositoryImpl
+    ): ColorInfoRepository
 
     @Binds
     @Singleton
